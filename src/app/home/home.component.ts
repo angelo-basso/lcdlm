7354d62bea91cd5638b4051cc370d5b0f046808c
Angelo BASSO <angelo67170@gmail.com>
2025-03-01 23:40:49 +0100

    Add commitment display with card component and service integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Commitment} from '../models/commitment.model';
+import {CommitmentService} from '../services/commitment.service';
+import {CommitmentCardComponent} from './commitment-card/commitment-card.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommitmentCardComponent,
+    NgForOf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  commitments!: Commitment[];
+
+  constructor(private commitmentService: CommitmentService) {
+  }
+
+  ngOnInit() {
+    this.commitments = this.commitmentService.commitments;
+  }
 }
