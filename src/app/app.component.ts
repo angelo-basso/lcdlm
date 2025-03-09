1fbc47de6cd33edd16ff9e51c8d839ef20192da6
Angelo BASSO <angelo67170@gmail.com>
2025-03-09 13:58:40 +0100

    Add scroll-to-top button functionality and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  showScrollToTopButton = false;
   title = 'lcdlm';
+
+  ngOnInit() {
+    window.addEventListener('scroll',()=>{
+      this.showScrollToTopButton = window.scrollY > 200;
+    });
+  }
 }
